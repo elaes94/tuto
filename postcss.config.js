3a1d09e2ed8593e9ad82654e3d1ddd0dff6d7444
elaes <traore-lassana@laposte.net>
2022-05-25 18:36:17 +0200

    add tailwindcss

---
+++ postcss.config.js
@@ -0,0 +1,15 @@
+let tailwindcss = require('tailwindcss');
+
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
+
+module.exports = {
+  plugins: [
+      tailwindcss('tailwind.config.js'), // your tailwind.js configuration file path
+      require('autoprefixer'),
+  ]
+}
