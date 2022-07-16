624106532c6a9344f0d4eb947cdd3039755f4c29
elaes <traore-lassana@laposte.net>
2022-07-16 19:03:48 +0200

    upgrade security

--- src/Controller/ActivityController.php
+++ src/Controller/ActivityController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/activity')]
+#[Route('/admin/activity')]
 class ActivityController extends AbstractController
 {
     #[Route('/', name: 'app_activity_index', methods: ['GET'])]
--- src/Controller/RegionController.php
+++ src/Controller/RegionController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/region')]
+#[Route('/admin/region')]
 class RegionController extends AbstractController
 {
     #[Route('/', name: 'app_region_index', methods: ['GET'])]
